f3c991b25273ab41ce29f51239b26172f6d6027b
Prajeesh Ag <prajeeshag@gmail.com>
2024-09-05 15:01:28 +0300

    First commit

---
+++ bin/get_forcing.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# shellcheck disable=SC2154,SC2086,SC1091,SC2034
+set -e
+
+source "$(dirname "$0")/koi"
+koiname="$0"
+koidescription="Extract a single ensemble member from the ECMWF S2S data"
+
+__koimain() {
+  __addarg "-h" "--help" "help" "optional" "" "$koidescription" ""
+  __addarg "-i" "--input" "storevalue" "required" "" "Input grib data" ""
+  __addarg "-m" "--member" "storevalue" "required" "" "Ensemble member" ""
+  __addarg "-o" "--output" "storevalue" "optional" "" "Output grib data" ""
+  __addarg "-n" "--nday" "storevalue" "optional" "46" "Forecast length in days" ""
+
+  __parseargs "$@"
+
+  set -u
+
+  inv=${input}.inv
+
+  if [[ "$member" -eq 0 ]]; then
+    search_string="Control forecast 0:"
+  else
+    search_string="Perturbed forecast $member:"
+  fi
+
+  tmpfile=data.mem${member}
+  grep "$search_string" <"$inv" | wgrib -i "$input" -s -grib -o "$tmpfile" >/dev/null
+
+  mkdir -p sorted
+  wgrib -s $tmpfile >inv
+
+  # Sort in time for ungrib compatibility
+  grep ":anl:" <inv | wgrib -i $tmpfile -s -grib -o "${tempfile}.0000" >/dev/null
+  for ((k = 1; k <= $((nday * 2)); k++)); do
+    j=$((k * 12))
+    jj=$(printf "%04d\n" $j)
+    grep ":${j}hr fcst:" <inv | wgrib -i "$tmpfile" -s -grib -o "${tempfile}.${jj}" >/dev/null
+  done
+  cat "${tmpfile}".* >"sorted_${tmpfile}"
+  rm inv "${tmpfile}".*
+
+  # Get land-sea mask
+  grep LSM <"$inv" | wgrib -s -grib -i "${input}" -o lsm.grb
+
+  # Patching SST with buffer zone so that metgrid interplotes it correctly
+  cdo -s -fillmiss2 -ifthen -ltc,0.01 lsm.grb -selvar,var34 "sorted_$tmpfile" "SST_$tmpfile"
+
+  cdo -s -replace "sorted_$tmpfile" "SST_$tmpfile" "$output"
+}
+
+__koirun "$@"
